6b052efd5ecee6ae1666eda7271f7f3e1fcf09c6
jeanpierreba <2713@holbertonschool.com>
2021-04-27 17:36:50 +0000

    Changed abs in statements to variable

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,6 +7,7 @@ else:
     last_digit = abs(number) % 10
 
 print("Last digit of {} is {} ".format(number, last_digit), end="")
+
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:

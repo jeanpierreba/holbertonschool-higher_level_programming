c26da3739d600bfde722aefeaff2275efdd9e35c
jeanpierreba <2713@holbertonschool.com>
2021-04-27 17:30:26 +0000

    Fixed errors with format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {} is {} ".format(number, abs(number) % 10), end="")
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = abs(number) % 10
+
+print("Last digit of {} is {} ".format(number, last_digit), end="")
 if abs(number) % 10 == 0:
-    print("and is 0".format(number, abs(number) % 10))
+    print("and is 0")
 elif abs(number) % 10 > 5:
-    print("and is greater than 5".format(number, abs(number) % 10))
+    print("and is greater than 5")
 elif abs(number) % 10 < 6 != 0:
-    print("and is less than 6 and not 0".format(number, abs(number) % 10))
+    print("and is less than 6 and not 0")
--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if not(i % 3) and not(i % 5):
-            print("FizzBuzz ".format(i), end="")
+            print("FizzBuzz ", end="")
         elif not(i % 3):
             print("Fizz, ", end="")
         elif not(i % 5):

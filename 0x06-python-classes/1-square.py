1f60a0987c6d60e09958919fbe66f6dcef259b27
jeanpierreba <2713@holbertonschool.com>
2021-05-19 00:56:35 +0000

    Added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+
+""" Definition of the Square class
+Private instance attribute: size
+instantiation with size (no type/value verification) """
+
+
 class Square:
     """ Definition of the Square class
     Private instance attribute: size

c16af1943d84296d206419c688073bf6da3c72c3
jeanpierreba <2713@holbertonschool.com>
2021-06-02 23:27:36 +0000

    Added advanced task

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Class that inherits from an int """
+
+
+class MyInt(int):
+	""" Function that have == and != inverted """
+
+	def __eq__(self, other):
+		return super().__ne__(other)
+
+	def __ne__(self, other):
+		return super().__eq__(other)

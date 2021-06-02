9365fa10db711167925fe703a7cfd9cfafa387d8
jeanpierreba <2713@holbertonschool.com>
2021-06-02 23:28:37 +0000

    Added pep8 requeriments

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,10 +4,10 @@
 
 
 class MyInt(int):
-	""" Function that have == and != inverted """
+    """ Function that have == and != inverted """
 
-	def __eq__(self, other):
-		return super().__ne__(other)
+    def __eq__(self, other):
+        return super().__ne__(other)
 
-	def __ne__(self, other):
-		return super().__eq__(other)
+    def __ne__(self, other):
+        return super().__eq__(other)

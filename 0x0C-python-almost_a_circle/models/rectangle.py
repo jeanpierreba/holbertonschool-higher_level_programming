11d17460b9261d9a5ea33a214c46e0cb52e1ab19
jeanpierreba <2713@holbertonschool.com>
2021-06-06 19:41:29 +0000

    Added update and to_dictionary methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,13 +74,26 @@ class Rectangle(Base):
         return str("[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height))
 
-    def update(self, *args):
-        if len(args) >= 1:
-            self.id = args[0]
-            self.width = args[1]
-        if len(args) >= 2:
-            self.height = args[2]
-        if len(args) >= 3:
-            self.x = args[3]
-        if len(args) >= 4:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        dictionary = {'id': self.id, 'width': self.width,
+                      'height': self.height, 'x': self.x, 'y': self.y}
+        return dictionary
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,23 +5,45 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
 
-	def __init__(self, size, x=0, y=0, id=None):
-		self.size = size
-		super().__init__(size, size, x, y, id)
+    def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
+        super().__init__(size, size, x, y, id)
 
-	@property
-	def size(self):
-		return self.__size
+    @property
+    def size(self):
+        return self.__size
 
-	@size.setter
-	def size(size, value):
-		if not isinstance(value, int):
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-		self.__size = value
+        self.__size = value
 
-	def __str__(self):
-		return str("[Square] ({}) {}/{} - {}".format(
-			self.id, self.x, self.y, self.size
-		))
+    def __str__(self):
+        return str("[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.size
+        ))
+
+    def update(self, *args, **kwargs):
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().update(id=value)
+                else:
+                    setattr(self, key, value)
+
+    def to_dictionary(self):
+        dictionary = {'id': self.id, 'size': self.size,
+                      'x': self.x, 'y': self.y}
+        return dictionary

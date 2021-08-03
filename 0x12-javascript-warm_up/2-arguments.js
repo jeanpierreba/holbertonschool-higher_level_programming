8424c78acc7948ce1d61904d09267dc3f2179d39
jeanpierreba <2713@holbertonschool.com>
2021-08-02 21:50:48 -0500

    changed capital letter

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 if (args.length === 2) {
-  console.log('No Argument');
+  console.log('No argument');
 } else if (args.length === 3) {
   console.log('Argument found');
 } else {

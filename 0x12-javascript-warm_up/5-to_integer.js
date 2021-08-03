8ec95f37983a09e5e3bf32cb2a6ae61801b7eaa8
jeanpierreba <2713@holbertonschool.com>
2021-08-02 21:46:18 -0500

    capital M

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const args = process.argv;
 if (isNaN(args[2])) {
   console.log('Not a number');
 } else {
-  console.log('my number: ' + parseInt(args[2]));
+  console.log('My number: ' + parseInt(args[2]));
 }

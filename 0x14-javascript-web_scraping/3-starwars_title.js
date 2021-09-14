cc4f3598f5753dfeaad08482b82769739bcab4c6
jeanpierreba <2713@holbertonschool.com>
2021-09-14 16:07:10 -0500

    Changed url in reques

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const argv = process.argv;
 const request = require('request');
 
-request('http://swapi.co/api/films/' + argv[2], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, response, body) {
   if (error === null) {
     const json_body = JSON.parse(body);
     console.log(json_body.title)

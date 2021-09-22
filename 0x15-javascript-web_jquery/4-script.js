8ddaa1bb0fc157b1e71816fe801d9ed4ec66aa16
jeanpierreba <2713@holbertonschool.com>
2021-09-22 14:13:47 -0500

    Added jquery files

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,3 @@
+$('DIV#red_header').click(() => {
+  $('HEADER').toggleClass('green red');
+});
---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,3 @@
+$('DIV#red_header').click(function () {
+  $(<li>Item</li>).appendTo('UL.my_list');
+});
